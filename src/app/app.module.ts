a1733569d6df2f7a9ce5ef0872054b92f7905e0c
Sree1710 <kunjusree817@gmail.com>
2022-07-06 12:12:48 +0530

    departmentname component created and tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentnameComponent } from './departmentname/departmentname.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentnameComponent
   ],
   imports: [
     BrowserModule,
