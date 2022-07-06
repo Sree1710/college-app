d054c9294db12fef54f566c595e116172c2a6e12
Sree1710 <kunjusree817@gmail.com>
2022-07-06 13:05:52 +0530

    students component created and tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentnameComponent } from './departmentname/departmentname.component';
 import { FacultynameComponent } from './facultyname/facultyname.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartmentnameComponent,
-    FacultynameComponent
+    FacultynameComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
